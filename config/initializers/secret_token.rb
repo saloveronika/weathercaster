228e1a01e62566e247d3090016cd76aab8b235b5
Pavlo Mashchak <pmashchak@hercules.(none)>
2011-07-09 18:55:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheathercaster::Application.config.secret_token = '9978f2b21a6c37d0070b1d92363ac00c20dcab6578d7f6324ae44d823394ed4e5d5e413be57bc2d28d85159b4b82e4af1c8564dff06b89cad3b62fdbde46ff51'
